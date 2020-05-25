3cf4b84769f9c4d11666d399e80f494cf463e597
sujaygandham <sujay.gandham@gmail.com>
2020-05-24 23:03:11 -0500

    add nonce to emotion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/core";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+
+export const myCache = createCache({
+  nonce: "cG9ydGZvbGlvCg==",
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <CacheProvider value={myCache}>
+      <Router>
+        <App />
+      </Router>
+    </CacheProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
